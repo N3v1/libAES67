N3v1/libAES67

commit 97d5fe3ab22fbe13b5873ef60f307dc9f66ce056

Refactor libAES67 headers and test infrastructure
The include hierarchy has been cleaned up and <libAES67/platform.h> is required to be included first so platform-specific feature macros, such as DARWIN_C_SOURCE and _POSIX_C_SOURCE, are defined before any system headers are pulled in.

The safe multiplication and addition macros (LA_SAFE_MUL64, LA_SAFE_ADD64) have been fixed to remove the dependency on the previously required LA_USE_SAFE_MUL include guard fixing compilation issues using clang.

CMakeLists.txt now uses a glob pattern to collect all source files and sets the public include directories correctly for both build and install targets.

Signed-off-by: Nevio Hirani <n3v1010@gmail.com>
Reviewed-by: Nevio Hirani <n3v1010@gmail.com>

.gitignore
@@ -30,7 +30,6 @@ CMakeFiles/
 cmake-build-debug/
 CMakeCache.txt
 cmake_install.cmake
-Makefile
 
 # Xcode Build
 .build/
@@ -51,3 +50,6 @@ Makefile
 *.diff
 diff.txt
 
+*.asn
+*.asn1
+*.swp

include/libAES67/time.h
@@ -50,10 +50,10 @@
 
 #define LIBAES67_INTERNAL_INCLUDE 1
 
-#include <stdint.h>
+#include <libAES67/platform.h>
+#include <libAES67/__tai.h>
 
-#include "platform.h"
-#include "__tai.h"
+#include <stdint.h>
 
 #define LA_NS_PER_SEC        1000000000LL
 #define LA_SEC_PER_MIN       60

src/time.c
@@ -38,12 +38,14 @@
  * Description: Clock and time abstraction for PTP.
  */
 
+#include <libAES67/platform.h>
+#include <libAES67/time.h>
+
 #include <time.h>
 #include <errno.h>
-#include <_string.h>
-
-#define LA_USE_SAFE_MUL64
-#include "../include/libAES67/time.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
 
 LA_INLINE void la_normalize_timespec(struct timespec *ts) {
     ts->tv_sec += ts->tv_nsec / LA_NS_PER_SEC;

include/libAES67/__tai.h
@@ -48,7 +48,7 @@
 #error "Do not include this header directly. Use <libAES67/time.h>."
 #endif
 
-#include "platform.h"
+#include <libAES67/platform.h>
 
 #define LA_TAI_UTC_INITIAL_OFFSET        10
 #define LA_EXPECTED_FINAL_TAI_UTC_OFFSET 37

include/libAES67/endian.h
@@ -41,7 +41,9 @@
 #ifndef LIBAES67_ENDIAN_H
 #define LIBAES67_ENDIAN_H
 
-#idndef LIBAES67_INTERNAL_INCLUDE
+#include <libAES67/platform.h>
+
+#ifndef LIBAES67_INTERNAL_INCLUDE
 #error "Do not include this header directly."
 #endif
 
@@ -55,8 +57,6 @@
 #elif defined(__APPLE__) || defined(__FreeBSD__)
     #include <sys/endian.h>
 #endif
-
-#include "platform.h"
 
 #ifndef LA_BYTE_ORDER
 #if defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) && defined(__ORDER_BIG_ENDIAN__)

include/libAES67/platform.h
@@ -41,8 +41,18 @@
 #ifndef LIBAES67_PLATFORM_H
 #define LIBAES67_PLATFORM_H
 
+#if defined(__APPLE__)
+    #ifndef _DARWIN_C_SOURCE
+        #define _DARWIN_C_SOURCE
+    #endif
+#else
+    #ifndef _POSIX_C_SOURCE
+        #define _POSIX_C_SOURCE 200809L
+    #endif
+#endif
+
 #include <stdarg.h>
-#include <_stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #ifdef __cplusplus
@@ -111,67 +121,60 @@
     #define LA_API __attribute__((visibility("default")))
 #endif
 
-#ifdef LA_USE_SAFE_MUL64
-    #define LA_SAFE_MUL64(a, b, out)                        \
-        do {                                                \
-            if (__builtin_mul_overflow((a), (b), out)) {    \
-                errno = ERANGE;                             \
-                return -1;                                  \
-            }                                               \
-        } while (0)
+#define LA_SAFE_MUL64(a, b, out)                           \
+       do {                                                \
+           if (__builtin_mul_overflow((a), (b), out)) {    \
+               errno = ERANGE;                             \
+               return -1;                                  \
+           }                                               \
+       } while (0)
 
-    #define LA_SAFE_ADD64(a, b, out)                        \
-        do {                                                \
-            if (__builtin_add_overflow((a), (b), out)) {    \
-                errno = ERANGE;                             \
-                return -1;                                  \
-            }                                               \
-        } while (0)
-#else
-#error "LA_USE_SAFE_MUL not defined, cannot use SAFE_MUL/ADD macros"
-#endif
+#define LA_SAFE_ADD64(a, b, out)                           \
+       do {                                                \
+           if (__builtin_add_overflow((a), (b), out)) {    \
+               errno = ERANGE;                             \
+               return -1;                                  \
+           }                                               \
+       } while (0)
 
 __LA_BEGIN_C_DECLS
 
 #define xasprintf(strp, ...) la_xasprintf((strp), ##__VA_ARGS__)
 
 LA_INLINE int la_xasprintf(char **strp, const char *fmt, ...) {
+    if (!strp || !fmt) { return -1; }
+
     va_list args;
     va_start(args, fmt);
 
-#if defined(__GLIBC__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-    const int ret = vasprintf(strp, fmt, args);
-    va_end(args);
-    return ret;
-#else
     va_list args_copy;
     va_copy(args_copy, args);
 
-    int len = vsnprintf(NULL, 0, fmt, args);
+    int len = vsnprintf(NULL, 0, fmt, args_copy);
+    va_end(args_copy);
+
     if (len < 0) {
-        va_end(args_copy);
         va_end(args);
         return -1;
     }
 
     *strp = malloc(len + 1);
     if (!*strp) {
-        va_end(args_copy);
         va_end(args);
         return -1;
     }
 
+    va_copy(args_copy, args);
     int ret = vsnprintf(*strp, (size_t)len + 1, fmt, args_copy);
+    va_end(args_copy);
+    va_end(args);
+
     if (ret < 0) {
         free(*strp);
         *strp = NULL;
     }
 
-    va_end(args_copy);
-    va_end(args);
-
     return ret;
-#endif
 }
 
 __LA_END_C_DECLS

libAES67.c
@@ -38,6 +38,5 @@
  * Description:
  */
 
+#include "libAES67/platform.h"
 #include "libAES67.h"
-
-#include <stdio.h>

tests/CMakeLists.txt
@@ -36,34 +36,17 @@
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ##===----------------------------------------------------------------------===##
 
-cmake_minimum_required(VERSION 4.1)
-project(libAES67 C CXX)
+file(GLOB TEST_SRCS *.c)
 
-set(CMAKE_C_STANDARD 23)
-set(CMAKE_C_STANDARD_REQUIRED ON)
+foreach(test_src ${TEST_SRCS})
+    get_filename_component(test_name ${test_src} NAME_WE)
 
-enable_testing()
+    add_executable(${test_name} ${test_src})
+    target_link_libraries(${test_name} PRIVATE libAES67)
 
-option(BUILD_SHARED_LIBS "Build shared libraries instead of static ones" OFF)
+    if(UNIX AND NOT APPLE)
+        target_link_libraries(test_time rt)
+    endif()
 
-file(GLOB LIBAES67_SRCS
-        src/*.c
-)
-
-add_library(libAES67 ${LIBAES67_SRCS})
-
-target_include_directories(libAES67
-        PUBLIC
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-        $<INSTALL_INTERFACE:include>
-)
-
-if (WIN32)
-    message(FATAL_ERROR "Windows builds are not supported yet.")
-endif()
-
-if (MSVC)
-    message(FATAL_ERROR "MSVC compiler is not supported.")
-endif()
-
-add_subdirectory(tests)
+    add_test(NAME ${test_name} COMMAND ${test_name})
+endforeach()

commit b3437ccbdb4aa574f623cb13d18ad452b6bbf1a0

(time): Fix overflow checks and add la_time_from_ns() tests
Tighten overflow handling in la_time_from_ns() by performing comparisons in the correct integer domain and avoiding premature narrowing during day calculation.

Add convenience time macros (LA_SEC, LA_MIN, LA_HOUR and LA_DAY) and LA_NS_PER_HALF_SEC.

Signed-off-by: Nevio Hirani <n3v1010@gmail.com>
Fixes: 97d5fe3a

.gitignore
@@ -19,6 +19,7 @@
 *.exe
 *.out
 *.app
+tests/time_test
 
 # Debug files
 *.dSYM/

include/libAES67/time.h
@@ -56,9 +56,15 @@
 #include <stdint.h>
 
 #define LA_NS_PER_SEC        1000000000LL
+#define LA_NS_PER_HALF_SEC   500'000'000ULL
 #define LA_SEC_PER_MIN       60
 #define LA_MIN_PER_HOUR      60
 #define LA_HOUR_PER_DAY      24
+
+#define LA_SEC(x)   ((uint64_t)(x))
+#define LA_MIN(x)   (LA_SEC(x) * 60ULL)
+#define LA_HOUR(x)  (LA_MIN(x) * 60ULL)
+#define LA_DAY(x)   (LA_HOUR(x) * 24ULL)
 
 /**
  * @def LA_CLOCK_1588

src/time.c
@@ -527,7 +527,7 @@ int la_time_from_ns(la_time_t *out, const uint64_t nsec) {
     const uint64_t total_sec = nsec / LA_NS_PER_SEC;
     const int32_t nsec_rem   = (int32_t)(nsec % LA_NS_PER_SEC);
 
-    if (total_sec > INT64_MAX) {
+    if (total_sec > (uint64_t)INT64_MAX) {
         errno = ERANGE;
         return -1;
     }
@@ -536,13 +536,14 @@ int la_time_from_ns(la_time_t *out, const uint64_t nsec) {
 
     const int64_t secs_per_day    = (int64_t)LA_HOUR_PER_DAY * LA_MIN_PER_HOUR * LA_SEC_PER_MIN;
     const int64_t secs_per_hour   = (int64_t)LA_MIN_PER_HOUR * LA_SEC_PER_MIN;
-    const int64_t secs_per_minute = LA_SEC_PER_MIN;
+    const int64_t secs_per_minute = (int64_t)LA_SEC_PER_MIN;
 
-    const int32_t day    = (int32_t)(sec / secs_per_day);
-    if ((int64_t)day > INT32_MAX) {
+    const int64_t day64 = sec / secs_per_day;
+    if (day64 > INT32_MAX) {
         errno = ERANGE;
         return -1;
     }
+    const  int32_t day = (int32_t)day64;
     sec %= secs_per_day;
 
     const int32_t hour   = (int32_t)(sec / secs_per_hour);